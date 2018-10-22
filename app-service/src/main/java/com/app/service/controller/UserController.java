cc56e83489aa80a6b31b7bcb3e7d8c11a12e138a
vedron <vedron@163.com>
2018-10-22 11:55:39 +0800

    userinfo 接口

--- app-service/src/main/java/com/app/service/controller/UserController.java
+++ app-service/src/main/java/com/app/service/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.common.message.StatusCode;
 import com.app.common.redis.RedisKeyPrefix;
 import com.app.common.redis.RedisUtil;
 import com.app.dao.UserMapper;
@@ -30,7 +31,12 @@ public class UserController{
 	@RequestMapping(value = "/getUserInfo", method = RequestMethod.GET)
 	public Resp getUserInfo(@RequestHeader(value="userId") String userId) {
     	log.info("getUserInfo: " + userId);
+    	if (userId == null) {
+    		return new Resp(StatusCode.BAD_REQUEST.value());
+    	}
+    	
     	UserInfoDto user = new UserInfoDto();
+    	user.setUserId(Long.valueOf(userId));
     	user.setUserDeviceId(redisService.hGet(RedisKeyPrefix.USER_INFO_ + userId, "userDeviceId"));
     	user.setUserMobilephone(redisService.hGet(RedisKeyPrefix.USER_INFO_ + userId, "userMobilephone"));
     	user.setUserDeviceType(redisService.hGet(RedisKeyPrefix.USER_INFO_ + userId, "userDeviceType"));
